e90229a745ccbac0fb0d60270784ab62632cf942
andantet <orrdaniel73@gmail.com>
2022-04-12 16:55:30 +0100

    get rid of some redirects :)

--- frame-content-registries-v0/src/main/java/net/moddingplayground/frame/mixin/contentregistries/LadderStateRegistryMixins.java
+++ frame-content-registries-v0/src/main/java/net/moddingplayground/frame/mixin/contentregistries/LadderStateRegistryMixins.java
@@ -1,77 +1,71 @@
 package net.moddingplayground.frame.mixin.contentregistries;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.LadderBlock;
+import net.minecraft.block.TrapdoorBlock;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageTracker;
-import net.minecraft.fluid.FlowableFluid;
+import net.minecraft.util.math.BlockPos;
 import net.moddingplayground.frame.api.contentregistries.v0.StateRegistry;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Optional;
 
 public class LadderStateRegistryMixins {
     @Mixin(DamageTracker.class)
     public static class DamageTrackerMixin {
-        @Redirect(
+        @Shadow @Final private LivingEntity entity;
+        @Shadow @Nullable private String fallDeathSuffix;
+
+        @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+        @Inject(
             method = "setFallDeathSuffix",
             at = @At(
                 value = "INVOKE",
-                target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"
+                target = "Ljava/util/Optional;isPresent()Z",
+                shift = At.Shift.BEFORE
             ),
-            slice = @Slice(
-                to = @At(
-                    value = "FIELD",
-                    target = "Lnet/minecraft/block/Blocks;LADDER:Lnet/minecraft/block/Block;",
-                    shift = At.Shift.AFTER
-                )
-            )
+            locals = LocalCapture.CAPTURE_FAILHARD,
+            cancellable = true
         )
-        private boolean redirectSetFallDeathSuffixIsOf(BlockState state, Block block) {
-            if (block == Blocks.LADDER && StateRegistry.LADDERS_DEATH_MESSAGES.contains(state)) return true;
-            if (block == Blocks.SCAFFOLDING && StateRegistry.SCAFFOLDING_DEATH_MESSAGES.contains(state)) return true;
-            return state.isOf(block);
-        }
-    }
-
-    @Mixin(FlowableFluid.class)
-    public static class FlowableFluidMixin {
-        @Redirect(
-            method = "canFill",
-            at = @At(
-                value = "INVOKE",
-                target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"
-            ),
-            slice = @Slice(
-                to = @At(
-                    value = "FIELD",
-                    target = "Lnet/minecraft/block/Blocks;LADDER:Lnet/minecraft/block/Block;",
-                    shift = At.Shift.BY,
-                    by = 3
-                )
-            )
-        )
-        private boolean redirectCanFillIsOf(BlockState state, Block block) {
-            if (block == Blocks.LADDER && StateRegistry.LADDERS.contains(state)) return true;
-            return state.isOf(block);
+        private void fixFallDeathSuffix(CallbackInfo ci, Optional<BlockPos> pos) {
+            pos.map(this.entity.world::getBlockState)
+               .ifPresent(state -> {
+                   if (StateRegistry.LADDERS_DEATH_MESSAGES.contains(state)) {
+                       this.fallDeathSuffix = "ladder";
+                       ci.cancel();
+                   } else if (StateRegistry.SCAFFOLDING_DEATH_MESSAGES.contains(state)) {
+                       this.fallDeathSuffix = "scaffolding";
+                       ci.cancel();
+                   }
+               });
         }
     }
 
     @Mixin(LivingEntity.class)
     public static class LivingEntityMixin {
-        @Redirect(
+        @Inject(
             method = "canEnterTrapdoor",
             at = @At(
                 value = "INVOKE",
-                target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"
-            )
+                target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z",
+                shift = At.Shift.BEFORE
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD,
+            cancellable = true
         )
-        private boolean redirectCanEnterTrapdoorIsOf(BlockState state, Block block) {
-            if (block == Blocks.LADDER && (StateRegistry.LADDERS.contains(state) && state.contains(LadderBlock.FACING))) return true;
-            return state.isOf(block);
+        private void fixCanEnterTrapdoor(BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> cir, BlockState stated) {
+            if (StateRegistry.LADDERS.contains(stated) && stated.contains(LadderBlock.FACING)) {
+                if (stated.get(LadderBlock.FACING) == state.get(TrapdoorBlock.FACING)) cir.setReturnValue(true);
+            }
         }
     }
 }
