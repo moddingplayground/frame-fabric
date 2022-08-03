86e9f5dfa1fa528e9b920fe3c842c4b6705182f3
andantet <orrdaniel73@gmail.com>
2022-08-03 12:38:15 +0100

    fix testmod... kinda?

--- frame-enchantments-v0/src/testmod/java/net/moddingplayground/frame/test/enchantments/target/FrameTestEnchantmentTarget.java
+++ frame-enchantments-v0/src/testmod/java/net/moddingplayground/frame/test/enchantments/target/FrameTestEnchantmentTarget.java
@@ -1,12 +1,10 @@
 package net.moddingplayground.frame.test.enchantments.target;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 import net.moddingplayground.frame.api.enchantment.v0.target.CustomEnchantmentTarget;
 
+@SuppressWarnings("unused")
 public class FrameTestEnchantmentTarget extends CustomEnchantmentTarget {
-    @Override
-    public boolean isCustomAcceptableItem(Item item) {
+    /*public boolean isAcceptableItem(Item item) {
         return item == Items.SPONGE;
-    }
+    }*/
 }
